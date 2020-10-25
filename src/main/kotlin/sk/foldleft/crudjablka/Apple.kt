f330dc66a86ef17870c7c425c7f00944fde3cfef
Jakub Dubovsky <jakub.dubovsky@seznam.cz>
2020-10-25 19:28:00 +0100

    fix entity

--- src/main/kotlin/sk/foldleft/crudjablka/Apple.kt
+++ src/main/kotlin/sk/foldleft/crudjablka/Apple.kt
@@ -4,6 +4,7 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
+@Entity
 class Apple(var color: String, var size: Int) {
   @Id
   @GeneratedValue
