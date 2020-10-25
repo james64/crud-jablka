0fd7ec71814fbf3b94770ae81ec6a149d0ecabdc
Jakub Dubovsky <jakub.dubovsky@seznam.cz>
2020-10-25 16:25:32 +0100

    initial commit

---
+++ src/main/kotlin/sk/foldleft/crudjablka/CrudJablkaApplication.kt
@@ -0,0 +1,11 @@
+package sk.foldleft.crudjablka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudJablkaApplication
+
+fun main(args: Array<String>) {
+	runApplication<CrudJablkaApplication>(*args)
+}
